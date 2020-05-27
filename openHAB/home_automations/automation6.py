e1dc9d69f02a33973a5b7f50a89e7b432dde2cca
Humberto Rodríguez A <rhumbertgz@users.noreply.github.com>
2020-05-27 04:07:17 +0200

    Update tags

--- openHAB/home_automations/automation6.py
+++ openHAB/home_automations/automation6.py
@@ -8,7 +8,7 @@ from core.actions import PersistenceExtensions
 @rule("Power: Main energy monitor")                                                                                                         # green
 @when("Item HEM1_Total_Energy_Cleaned changed")                                                                                             # green
 def mainsEnergyMonitor(event):
-    lastThreeWeeksUsage = float(str(PersistenceExtensions.sumSince(ir.getItem("HEM1_Total_Energy_Delta"), DateTime.now().minusDays(21))))   # blue
+    lastThreeWeeksUsage = float(str(PersistenceExtensions.sumSince(ir.getItem("HEM1_Total_Energy_Delta"), DateTime.now().minusDays(21))))   # blue yellow
     if lastThreeWeeksUsage > 200:                                                                                                           # green  
        logDebug("Demo8","Send alert - {} => {}".format(event.itemName, str(event.itemState)))
             
